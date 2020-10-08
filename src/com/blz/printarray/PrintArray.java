318c261444a1f09e0d228442cfe88cb20533ca64
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-08 16:13:32 +0530

    [Akash]Added: toPrint() methods to print al the elements of their corresponding arrays

--- src/com/blz/printarray/PrintArray.java
+++ src/com/blz/printarray/PrintArray.java
@@ -1,7 +1,28 @@
 package com.blz.printarray;
 
 public class PrintArray {
+	private static void toPrint(Integer[] array) {
+		for (int element : array)
+			System.out.println(element);
+	}
+
+	private static void toPrint(Double[] array) {
+		for (double element : array)
+			System.out.println(element);
+	}
+
+	private static void toPrint(Character[] array) {
+		for (char element : array)
+			System.out.println(element);
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generics");
+		Integer[] intArray = { 1, 2, 3 };
+		Double[] doubleArray = { 1.0, 2.0, 3.0 };
+		Character[] charArray = { 'a', 'b', 'c' };
+		toPrint(intArray);
+		toPrint(doubleArray);
+		toPrint(charArray);
 	}
 }
