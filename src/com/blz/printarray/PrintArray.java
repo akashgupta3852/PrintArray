6c7b59a9ff7b58abdc74a2f384d6087a437aec42
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-08 16:25:06 +0530

    [Akash]Added: printing the elements using generics

--- src/com/blz/printarray/PrintArray.java
+++ src/com/blz/printarray/PrintArray.java
@@ -1,7 +1,18 @@
 package com.blz.printarray;
 
 public class PrintArray {
+	private static <T> void toPrint(T[] array) {
+		for (T element : array)
+			System.out.println(element);
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generics");
+		Integer[] intArray = { 1, 2, 3 };
+		Double[] doubleArray = { 1.0, 2.0, 3.0 };
+		Character[] charArray = { 'a', 'b', 'c' };
+		toPrint(intArray);
+		toPrint(doubleArray);
+		toPrint(charArray);
 	}
 }
